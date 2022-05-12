9038f83203e9cc8ee393fee24df9217137ba3f90
kykurniawan <contact.rizkykurniawan@gmail.com>
2022-05-12 15:47:54 +0800

    setup tailwindcss

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dapur Pribadi</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    @yield('body')
+</body>
+
+</html>
