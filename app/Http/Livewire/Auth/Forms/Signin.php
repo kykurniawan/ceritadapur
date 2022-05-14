796304a4625827c64e997be4d3a69a1e1db2289f
kykurniawan <contact.rizkykurniawan@gmail.com>
2022-05-14 23:55:58 +0800

    add some livewire functionality to signin form

--- app/Http/Livewire/Auth/Forms/Signin.php
+++ app/Http/Livewire/Auth/Forms/Signin.php
@@ -6,6 +6,25 @@ use Livewire\Component;
 
 class Signin extends Component
 {
+    public string $email = '';
+    public string $password = '';
+
+    protected array $rules = [
+        'email' => 'required|email',
+        'password' => 'required'
+    ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function submit()
+    {
+        $this->validate();
+        $this->reset();
+    }
+
     public function render()
     {
         return view('livewire.auth.forms.signin');
--- resources/views/livewire/auth/forms/signin.blade.php
+++ resources/views/livewire/auth/forms/signin.blade.php
@@ -1,14 +1,25 @@
 <div>
     <div class="p-5 bg-white rounded-lg border shadow mb-5">
-        <div class="mb-3">
-            <input type="email" name="" id="" class="form-input block w-full rounded-lg" placeholder="Alamat Email...">
-        </div>
-        <div class="mb-3">
-            <input type="password" name="" id="" class="form-input block w-full rounded-lg" placeholder="Kata Sandi...">
-        </div>
-        <div class="mb-3">
-            <button class="primary-button w-full">Masuk</button>
-        </div>
+        <form wire:submit.prevent="submit">
+            @csrf
+            <div class="mb-3">
+                <input type="email" wire:model="email" id="signin.email" class="form-input block w-full rounded-lg"
+                    placeholder="Alamat Email...">
+                @error('email')
+                    <span class="text-red-500">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <input type="password" wire:model="password" id="signin.password"
+                    class="form-input block w-full rounded-lg" placeholder="Kata Sandi...">
+                @error('password')
+                    <span class="text-red-500">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <button type="submit" class="primary-button w-full">Masuk</button>
+            </div>
+        </form>
         <div class="mb-3 text-center">
             <p class="text-gray-500 font-semibold">Atau gunakan metode lain</p>
         </div>
