1cff733f26880c204b0c3bc5a42ae5b00639bb5f
kykurniawan <contact.rizkykurniawan@gmail.com>
2022-05-14 18:55:33 +0800

    change page title to Cerita Dapur

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dapur Pribadi</title>
+    <title>Cerita Dapur</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
